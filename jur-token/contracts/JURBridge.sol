a01d81b2a186a164fee300d9ca14af6d518cc5ab
Adrian Coutsoftides <adrian.coutsoftides-ext@vitrifi.net>
2023-02-04 15:17:50 +0000

    ergonomic change to bridge contract to view balances

--- jur-token/contracts/JURBridge.sol
+++ jur-token/contracts/JURBridge.sol
@@ -6,6 +6,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract JURBridge {
 
   ERC20 public token;
+  mapping (address => uint256) balances;
+
   event SwapInitiated(address indexed from, uint256 value);
 
   constructor(ERC20 _token) {
@@ -15,6 +17,11 @@ contract JURBridge {
   function transfer(address recipient, uint256 amount) public {
     require(recipient != address(this));
     require(token.transferFrom(recipient, address(this), amount), "Transfer failed");
+    balances[recipient] += amount;
     emit SwapInitiated(recipient, amount);
+  }
+
+  function balanceOf(address target) public view returns(uint256) {
+    return balances[target];
   }
 }
